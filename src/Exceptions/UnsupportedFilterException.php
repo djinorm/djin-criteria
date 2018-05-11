7b05e84f8b0a5203458e6eed99e4cd67fa88d8b6
XAKEPEHOK <icq-475204@ya.ru>
2018-05-11 16:14:04 +0300

    UnsupportedFilterException

---
+++ src/Exceptions/UnsupportedFilterException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created for djin-filter-sort-paginate.
+ * Datetime: 11.05.2018 16:12
+ * @author Timur Kasumov aka XAKEPEHOK
+ */
+
+namespace DjinORM\Components\FilterSortPaginate\Exceptions;
+
+
+class UnsupportedFilterException extends \Exception
+{
+
+}
